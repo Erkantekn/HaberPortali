5770125eeb559141094a87f6c34b3043c9ca2097
Erkantekn <88429640+ErkanteknPomelo@users.noreply.github.com>
2022-01-03 14:48:52 +0300

    güncel

--- NewsPortal/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Admin/EditorCategory.razor.g.cs
+++ NewsPortal/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Admin/EditorCategory.razor.g.cs
@@ -12,42 +12,42 @@ namespace NewsPortal.Pages.Admin
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 1 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 2 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 4 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 5 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 6 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using NewsPortal.Models;
 
 #line default
@@ -63,7 +63,7 @@ using NewsPortal.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 45 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\EditorCategory.razor"
+#line 45 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\EditorCategory.razor"
                 public PortalDbContext Repository => Service;
 
             [Inject]
--- NewsPortal/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Admin/NewsPage.razor.g.cs
+++ NewsPortal/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Admin/NewsPage.razor.g.cs
@@ -12,49 +12,49 @@ namespace NewsPortal.Pages.Admin
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 1 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 2 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 4 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 5 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\_Imports.razor"
+#line 6 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\_Imports.razor"
 using NewsPortal.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\NewsPage.razor"
+#line 3 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\NewsPage.razor"
 using System.IO;
 
 #line default
@@ -69,7 +69,7 @@ using System.IO;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 70 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal for somee\NewsPortal\Pages\Admin\NewsPage.razor"
+#line 70 "D:\Ders\3.Sınıf\Web Programlama\Final\NewsPortal new\NewsPortal\Pages\Admin\NewsPage.razor"
         public PortalDbContext Repository => Service;
 
     public IEnumerable<News> NewsData { get; set; }
